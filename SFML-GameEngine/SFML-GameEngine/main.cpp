b5ee2a85dec03c7f11d79794ebaa7f0c322c7578
Hayden Goldstien <haydengoldstien@gmail.com>
2014-08-20 21:26:34 -0500

    Master: Inital Commit

---
+++ SFML-GameEngine/SFML-GameEngine/main.cpp
@@ -0,0 +1,10 @@
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+// Here is a small helper for you ! Have a look.
+#include "ResourcePath.hpp"
+
+int main(int, char const**)
+{
+    return EXIT_SUCCESS;
+}
