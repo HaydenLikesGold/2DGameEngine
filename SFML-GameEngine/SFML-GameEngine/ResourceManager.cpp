6c1bd8482ba12a8bc45dbc92f29d75bb81595af9
Hayden Goldstien <haydengoldstien@gmail.com>
2014-08-26 12:58:27 -0500

    ResourceManager: Fill out manager header file

---
+++ SFML-GameEngine/SFML-GameEngine/ResourceManager.cpp
@@ -0,0 +1,9 @@
+//
+//  AssetManager.cpp
+//  SFML-GameEngine
+//
+//  Created by Hayden Goldstien on 8/26/14.
+//  Copyright (c) 2014 HaydenGoldstien. All rights reserved.
+//
+
+#include "ResourceManager.hpp"
---
+++ SFML-GameEngine/SFML-GameEngine/ResourceManager.hpp
@@ -0,0 +1,27 @@
+//
+//  AssetManager.h
+//  SFML-GameEngine
+//
+//  Created by Hayden Goldstien on 8/26/14.
+//  Copyright (c) 2014 HaydenGoldstien. All rights reserved.
+//
+
+#ifndef __SFML_GameEngine__ResourceManager__
+#define __SFML_GameEngine__ResourceManager__
+
+#include <map>
+#include <string>
+
+template <typename Resource, typename Identifier>
+class ResourceManager
+{
+public:
+    void        load(Identifier id, const std::string& filenmae);
+    Resource&   get(Identifier id);
+    
+private:
+    void insertResource(Identifier id, std::unique_ptr<Resource> resource);
+    std::map<Identifier, std::unique_ptr<Resource>> mResourceMap;
+};
+
+#endif /* defined(__SFML_GameEngine__AssetManager__) */
