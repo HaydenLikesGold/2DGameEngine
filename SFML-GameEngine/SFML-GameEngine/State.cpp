1dabc543cdffba933d3df37083e5d358234c3038
Hayden Goldstien <haydengoldstien@gmail.com>
2014-09-01 11:59:26 -0500

    States: Remove Files, needs rethinking

--- SFML-GameEngine/SFML-GameEngine/State.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  State.cpp
-//  SFML-GameEngine
-//
-//  Created by Hayden Goldstien on 8/28/14.
-//  Copyright (c) 2014 HaydenGoldstien. All rights reserved.
-//
-
-#include "State.hpp"
--- SFML-GameEngine/SFML-GameEngine/StateManager.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  StateManager.cpp
-//  SFML-GameEngine
-//
-//  Created by Hayden Goldstien on 8/28/14.
-//  Copyright (c) 2014 HaydenGoldstien. All rights reserved.
-//
-
-#include "StateManager.h"
